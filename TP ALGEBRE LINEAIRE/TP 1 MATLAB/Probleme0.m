d91784c94f08693254f793516d76c59c8827dba3
Forgeling <benoit.renault@insa-lyon.fr>
2015-10-06 16:29:32 +0200

    Update

--- TP ALGEBRE LINEAIRE/TP 1 MATLAB/Probleme0.m
+++ TP ALGEBRE LINEAIRE/TP 1 MATLAB/Probleme0.m
@@ -1,26 +1,26 @@
-function Probleme1()
+function Probleme0()
   A = zeros(450);
   B = zeros(450, 1);
-  
+ 
   for i=1:450
-    % Left Side 
+    % Left Side
     if(i >= 2 && i <= 14)
       A(i, i-1) = 1;
       A(i, i+1) = 1;
       A(i, i+165) = 1;
       A(i, i+15) = 1;
       A(i, i) = -4;
-    
-    % Right Side 
+   
+    % Right Side
     elseif(i >= 437 && i <= 449)
       A(i, i-1) = 1;
       A(i, i+1) = 1;
       A(i, i-270) = 1;
       A(i, i-15) = 1;
       A(i, i) = -4;
-    
-    % Bold Line 
-    elseif(i >= 182 && i <= 194)
+   
+    % Bold Line
+    elseif(i >= 167 && i <= 179)
       A(i, i-1) = 1;
       A(i, i+1) = 1;
       A(i, i-15) = 1;
@@ -28,22 +28,22 @@ function Probleme1()
       A(i, i-165) = 1;
       A(i, i+270) = 1;
       A(i, i) = -6;
-    
-    % Red Points 
-    elseif(i == 248 || i == 263 || i == 278)
+   
+    % Red Points
+    elseif(i == 233 || i == 263-15 || i == 278-15)
       A(i, i) = 1;
       B(i) = 100;
-    
+   
     % Green Points
-    elseif((i >= 319 && i <= 327) || (i >= 334 && i <= 342))
+    elseif((i >= 319-15 && i <= 327-15) || (i >= 334-15 && i <= 342-15))
       A(i, i) = 1;
       B(i) = 10;
-    
+   
     % Blue Points (top and bottom sides)
     elseif(mod(i-1, 15) == 0 || mod(i-15, 15) == 0)
       A(i, i) = 1;
       B(i) = 50;
-    
+   
     % General case
     else
       A(i, i-1) = 1;
@@ -53,11 +53,11 @@ function Probleme1()
       A(i, i) = -4;
     end
   end
-  
-  A = A(1:450,1:450);
-  
+ 
+  %A = A(1:450,1:450);
+ 
   % Solving
-  X = GaussSeidel(A,B, 10);
+  X = GaussSeidel(A,B, 0.0001);
   Y = reshape(X, 15, 30);
   surf(Y);
 end
