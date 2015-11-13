1c623e7f71cc8df4c810eeab40f6e2bfd5ca586f
Forgeling <benoit.renault@insa-lyon.fr>
2015-11-13 16:03:42 +0100

    Adding functions, for real this time

--- TP FOURIER/x3.m
+++ TP FOURIER/x3.m
@@ -1,3 +1,15 @@
 function [ret] = x3(t,C,f0,beta,tho,deltaT)
     ret = exp(-beta*t)*heaviside(t);
 end
+
+function [ret] = heaviside(t)
+    if (t < 0)
+        ret = 0;
+    end
+    if (t = 0)
+        ret = 0.5;
+    end
+    if (t > 0)
+        ret = 1;
+    end
+end
--- TP FOURIER/x4.m
+++ TP FOURIER/x4.m
@@ -1,3 +1,9 @@
 function [ret] = x4(t,C,f0,beta,tho,deltaT)
     ret = dirac(t - deltaT);
 end
+
+function [ret] = dirac(t)
+    if (abs(t) < 0.0000001)
+        ret = 1;
+    end
+end
