1098252d4ec8ec64670f8b1a41d153cf211bc25d
Le Poney <espeut.clement@gmail.com>
2015-11-13 17:44:53 +0100

    Redemem

--- TP FOURIER/x6p.m
+++ TP FOURIER/x6p.m
@@ -3,7 +3,7 @@ function [ret] = x6p(t,C,f0,beta,tho,deltaT)
 end
 
 function [ret] = rectangularPulse(tho,f0, t)
-    value = 1/2*sin(2 * pi() * f0 * t)+0.5;
+    value = 1/2*cos(2 * pi() * f0 * t)+0.5;
     if value >= 0.5
         ret = 1;
     else
