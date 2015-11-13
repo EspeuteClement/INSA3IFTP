6a8d400129e9de38787277aafdb78dd4a945f66f
Le Poney <espeut.clement@gmail.com>
2015-11-13 16:54:57 +0100

    des photos !

--- TP FOURIER/prepareFourierData.m
+++ TP FOURIER/prepareFourierData.m
@@ -1,12 +1,16 @@
-function [ret] = prepareFourierData(data,samples,lowerBound,higherBound,isReal)
+function [ret] = prepareFourierData(data,samples,lowerBound,higherBound,spectre)
     
     ret = zeros(2,samples);
     % Bake the ret
     
-    if isReal == 1
+    if strcmp(spectre,'real')
        ret(2,:) = real(data);
-    else
+    elseif strcmp(spectre,'imag')
        ret(2,:) = imag(data);
+    elseif strcmp(spectre,'mod')
+       ret(2,:) = abs(data);
+    elseif strcmp(spectre,'arg')
+       ret(2,:) = angle(data);
     end
     
     % Add the frequencies :
