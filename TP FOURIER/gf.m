3371ecbdfc3fcb9acdb8050755e6ed778f2d2e33
Forgeling <benoit.renault@insa-lyon.fr>
2015-11-20 15:44:20 +0100

    Add new assignment

--- TP FOURIER/gf.m
+++ TP FOURIER/gf.m
@@ -1,4 +1,4 @@
 function [ret] = gf(t,f)
     deltaF = 40;
-    ret =  sin(2*pi()*f*t) + 2.sin(2*pi()*(f + deltaF)*t) + 3.sin(2*pi()*(f + 2*deltaF)*t);
+    ret =  sin(2*pi()*f*t) + 2*sin(2*pi()*(f + deltaF)*t) + 3*sin(2*pi()*(f + 2*deltaF)*t);
 end
