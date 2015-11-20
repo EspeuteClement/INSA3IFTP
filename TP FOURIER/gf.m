f81ef17a4b24c309a0508ae242bd470be6240481
Le Poney <espeut.clement@gmail.com>
2015-11-20 15:15:25 +0100

    Ajout images tp fourier

--- TP FOURIER/gf.m
+++ TP FOURIER/gf.m
@@ -1,4 +1,4 @@
-function [ret] = gf(t,f)
+function [ret] = gf(t,f,f0,beta,tho,deltaT)
     deltaF = 40;
-    ret =  sin(2*pi()*f*t) + 2.sin(2*pi()*(f + deltaF)*t) + 3.sin(2*pi()*(f + 2*deltaF)*t);
+    ret =  sin(2*pi()*f*t) + 2*sin(2*pi()*(f + deltaF)*t) + 3*sin(2*pi()*(f + 2*deltaF)*t);
 end
